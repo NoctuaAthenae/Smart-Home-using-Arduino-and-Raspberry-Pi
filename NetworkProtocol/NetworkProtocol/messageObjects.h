47da5154b5f642d2d94907d0a6ff8eb0153a8dfb
Artemis <dev@olympier.de>
2025-05-23 00:19:06 +0200

    Added type return

--- NetworkProtocol/NetworkProtocol/messageObjects.h
+++ NetworkProtocol/NetworkProtocol/messageObjects.h
@@ -19,7 +19,7 @@ public:
     bool isGroup();
     void setGroup(bool);
     bool isGroupAscending();
-    bool setGroupAscending(bool);
+    void setGroupAscending(bool);
     void addChecksum();
 
     Message(uint8_t receiver, uint8_t lastDeviceId, uint8_t nextHop, uint8_t typeAndGroups) {
@@ -31,16 +31,15 @@ public:
         this->timestamp = 0;
         this->checksum = 0;
     }
-
-
-
 };
 
 class RegisterMessage : public Message {
 public:
     using Message::Message;
 
-    uint8_t getType() override;
+    uint8_t getType() override {
+        return 2;
+    }
 };
 
 class AcceptRejectMessage : public Message {
@@ -54,7 +53,9 @@ public:
     uint8_t givenId;
     bool isAccept;
 
-    uint8_t getType() override;
+    uint8_t getType() override {
+        return 3;
+    }
 };
 
 class RouteCreationMessage : public Message {
@@ -70,7 +71,9 @@ public:
     uint8_t newId;
     uint8_t route[21];
 
-    uint8_t getType() override;
+    uint8_t getType() override {
+        return 5;
+    }
 };
 
 class PingMessage : public Message {
@@ -86,7 +89,9 @@ public:
     uint8_t senderId;
     bool isResponse;
 
-    uint8_t getType() override;
+    uint8_t getType() override {
+        return 4;
+    }
 };
 
 class AddRemoveToGroupMessage : public Message {
@@ -100,7 +105,9 @@ public:
     bool isAddToGroup;
     uint8_t groupId;
 
-    uint8_t getType() override;
+    uint8_t getType() override {
+        return 6;
+    }
 };
 
 #endif //NETWORKPROTOCOL_MESSAGEOBJECTS_H
