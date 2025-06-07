8d945994090f0510e130a5b098d4e7610ceeeb96
Artemis <dev@olympier.de>
2025-06-08 00:07:16 +0200

    Added test for acceptReject

--- NetworkProtocol/NetworkProtocol/boostTests/CreateRawPackageTest.cpp
+++ NetworkProtocol/NetworkProtocol/boostTests/CreateRawPackageTest.cpp
@@ -89,4 +89,47 @@ BOOST_AUTO_TEST_CASE(RegisterRawPackageTest) {
 
 }
 
+
+BOOST_AUTO_TEST_CASE(AcceptRejectRawPackageTest) {
+    uint8_t id = std::rand() % 256;
+    uint8_t lastDevive = std::rand() % 256;
+    uint32_t timestamp = std::rand();
+    uint8_t nextHop = std::rand() % 256;
+    AcceptRejectMessage msg = AcceptRejectMessage(id, lastDevive, nextHop, false, false, true);
+
+    msg.timestamp = timestamp;
+    std::vector<uint8_t *> rawPackages = msg.getRawPackages();
+
+    BOOST_CHECK_EQUAL(rawPackages.size(), 1);
+
+    uint8_t *package = rawPackages.at(0);
+
+    BOOST_CHECK_EQUAL(package[0], NETWORKPROTOCOL_VERSION);
+    BOOST_CHECK_EQUAL(package[1], id);
+    BOOST_CHECK_EQUAL(package[2], lastDevive);
+    BOOST_CHECK_EQUAL(package[3], nextHop);
+    BOOST_CHECK_EQUAL(package[4] / 4, 3);
+
+    uint32_t createdTimestamp = 0;
+    memcpy(&createdTimestamp, package + 6, sizeof(uint32_t));
+
+    BOOST_CHECK_EQUAL(createdTimestamp, timestamp);
+
+    BOOST_CHECK(Message::checkChecksum(package));
+
+
+    auto* createdMsg = (AcceptRejectMessage*) Message::fromRawBytes(rawPackages.at(0));
+
+    BOOST_CHECK_EQUAL(createdMsg->version, NETWORKPROTOCOL_VERSION);
+    BOOST_CHECK_EQUAL(createdMsg->receiver, id);
+    BOOST_CHECK_EQUAL(createdMsg->lastDeviceId, lastDevive);
+    BOOST_CHECK_EQUAL(createdMsg->nextHop, nextHop);
+    BOOST_CHECK_EQUAL(createdMsg->getType(), 3);
+    BOOST_CHECK(createdMsg->isAccept);
+
+    package[2] = lastDevive + 1;
+    BOOST_CHECK(!Message::checkChecksum(package));
+
+}
+
 BOOST_AUTO_TEST_SUITE_END()
